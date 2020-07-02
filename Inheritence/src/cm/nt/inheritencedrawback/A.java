f23d4455366cd6be186cdea20f1f24279cb14946
PAVAN <PAVAN@DESKTOP-RFJHAM7>
2020-07-03 04:20:30 +0530

    Fourth commit

---
+++ Inheritence/src/cm/nt/inheritencedrawback/A.java
@@ -0,0 +1,16 @@
+package cm.nt.inheritencedrawback;
+
+public class A {
+	
+		
+	
+public int m1() {
+	return 786;
+}
+	public double m2()
+	{
+		return 56.56;
+	}
+	
+}
+	
---
+++ Inheritence/src/cm/nt/inheritencedrawback/B.java
@@ -0,0 +1,15 @@
+package cm.nt.inheritencedrawback;
+
+public class B extends A {
+
+		
+	
+		
+	
+	public int m3()
+	{
+		
+	 return  super.m1();
+	}
+	
+}
